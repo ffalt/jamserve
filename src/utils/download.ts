2db0d355771e34b6c69d5d06365be17225e47716
ffalt <ffalty@gmail.com>
2018-12-14 14:08:01 +0100

    fix(download helper): report everything other than 200 as error

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,16 +1,20 @@
 import request from 'request';
 import fs from 'fs';
+import * as http from 'http';
 
 export function downloadFile(url: string, filename: string): Promise<void> {
 	return new Promise<void>((resolve, reject) => {
-		request(url)
-			.pipe(fs.createWriteStream(filename))
-			.on('close', (err: Error) => {
-				if (err) {
-					reject(err);
+		request.get(url)
+			.on('error', (err: Error) => {
+				reject(err);
+			})
+			.on('complete', (res: { statusCode: number }) => {
+				if (res.statusCode !== 200) {
+					reject(new Error(http.STATUS_CODES[res.statusCode]));
 				} else {
 					resolve();
 				}
-			});
+			})
+			.pipe(fs.createWriteStream(filename));
 	});
 }
