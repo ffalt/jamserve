fbe6222a6984b9c896052351ff4f63705731afa4
ffalt <ffalty@gmail.com>
2018-12-26 14:23:58 +0100

    test(downloadService): use testStore mock objects

--- src/engine/download/download.service.test.ts
+++ src/engine/download/download.service.test.ts
@@ -2,33 +2,37 @@ import {assert, expect, should, use} from 'chai';
 import {after, before, beforeEach, describe, it} from 'mocha';
 import {testService} from '../../objects/base/base.service.spec';
 import {DownloadService} from './download.service';
-import {mockTrack} from '../../objects/track/track.mock';
 import {mockUser} from '../../objects/user/user.mock';
 import {mockFolder} from '../../objects/folder/folder.mock';
 import {mockEpisode} from '../../objects/episode/episode.mock';
 import {mockPlaylist} from '../../objects/playlist/playlist.mock';
-import {mockArtist} from '../../objects/artist/artist.mock';
-import {mockAlbum} from '../../objects/album/album.mock';
+import {Store} from '../store/store';
 
 describe('DownloadService', () => {
 	let downloadService: DownloadService;
+	let store: Store;
 	testService(
 		(storeTest, imageModuleTest) => {
+			store = storeTest.store;
 			downloadService = new DownloadService(storeTest.store.trackStore);
 		},
 		() => {
 			const user = mockUser();
 			user.id = 'downloadUserID1';
 			it('should download a track', async () => {
-				const track = mockTrack();
-				track.id = 'downloadTrackID1';
+				const track = await store.trackStore.random();
+				if (!track) {
+					return Promise.reject('Invalid Test Setup');
+				}
 				const res = await downloadService.getObjDownload(track, undefined, user);
 				should().exist(res);
 				should().exist(res.pipe);
 			});
 			it('should download a folder', async () => {
-				const folder = mockFolder();
-				folder.id = 'downloadFolderID1';
+				const folder = await store.folderStore.random();
+				if (!folder) {
+					return Promise.reject('Invalid Test Setup');
+				}
 				const res = await downloadService.getObjDownload(folder, undefined, user);
 				should().exist(res);
 				should().exist(res.pipe);
@@ -48,15 +52,19 @@ describe('DownloadService', () => {
 				await downloadService.getObjDownload(episode, undefined, user).should.eventually.be.rejectedWith(Error);
 			});
 			it('should download an artist', async () => {
-				const artist = mockArtist();
-				artist.id = 'downloadArtistID1';
+				const artist = await store.artistStore.random();
+				if (!artist) {
+					return Promise.reject('Invalid Test Setup');
+				}
 				const res = await downloadService.getObjDownload(artist, undefined, user);
 				should().exist(res);
 				should().exist(res.pipe);
 			});
 			it('should download an album', async () => {
-				const album = mockAlbum();
-				album.id = 'downloadAlbumID1';
+				const album = await store.albumStore.random();
+				if (!album) {
+					return Promise.reject('Invalid Test Setup');
+				}
 				const res = await downloadService.getObjDownload(album, undefined, user);
 				should().exist(res);
 				should().exist(res.pipe);
@@ -66,6 +74,11 @@ describe('DownloadService', () => {
 				playlist.id = 'downloadPlaylistID1';
 				playlist.isPublic = true;
 				playlist.userID = user.id;
+				const track = await store.trackStore.random();
+				if (!track) {
+					return Promise.reject('Invalid Test Setup');
+				}
+				playlist.trackIDs = [track.id];
 				let res = await downloadService.getObjDownload(playlist, undefined, user);
 				should().exist(res);
 				should().exist(res.pipe);
