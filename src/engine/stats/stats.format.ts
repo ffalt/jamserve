cb5f8212ed57190f485e0850226729113e98f7ef
ffalt <ffalty@gmail.com>
2019-04-16 14:57:24 +0200

    feat(api): more album types in stats

--- src/engine/stats/stats.format.ts
+++ src/engine/stats/stats.format.ts
@@ -5,16 +5,8 @@ export function formatStats(stats: Stats): Jam.Stats {
 	return {
 		rootID: stats.rootID,
 		album: stats.album,
-		albumTypes: {
-			album: stats.albumTypes.album,
-			compilation: stats.albumTypes.compilation,
-			audiobook: stats.albumTypes.audiobook
-		},
-		artistTypes: {
-			album: stats.artistTypes.album,
-			compilation: stats.artistTypes.compilation,
-			audiobook: stats.artistTypes.audiobook
-		},
+		albumTypes: stats.albumTypes,
+		artistTypes: stats.artistTypes,
 		artist: stats.artist,
 		folder: stats.folder,
 		track: stats.track
