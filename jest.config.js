3f08ecad3c670e630eb191c8c9fc0f89a8939e5a
ffalt <ffalty@gmail.com>
2019-10-28 09:51:49 +0100

    test(jest): ignore paths

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	coverageDirectory: 'coverage',
+	modulePathIgnorePatterns: ['<rootDir>/local/', '<rootDir>/coverage/', '<rootDir>/data', '<rootDir>/dist'],
 	testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(test).[jt]s?(x)"],
 	globals: {
 		_testDatabases_: ['nedb'] //, 'elastic']
