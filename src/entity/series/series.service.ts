f41098fca7124b3d94635f9be6aa39ce91741676
ffalt <ffalty@gmail.com>
2020-08-01 14:44:59 +0200

    fix(series): choose artist image if available

--- src/entity/series/series.service.ts
+++ src/entity/series/series.service.ts
@@ -1,8 +1,10 @@
 import {Inject, InRequestScope} from 'typescript-ioc';
 import {Series} from './series';
-import {ApiBinaryResult} from '../../modules/rest/builder/express-responder';
+import {ApiBinaryResult} from '../../modules/rest';
 import {FolderService} from '../folder/folder.service';
 import {Orm} from '../../modules/engine/services/orm.service';
+import {FolderType} from '../../types/enums';
+import {Folder} from '../folder/folder';
 
 @InRequestScope
 export class SeriesService {
@@ -10,7 +12,17 @@ export class SeriesService {
 	folderService!: FolderService;
 
 	async getImage(orm: Orm, series: Series, size?: number, format?: string): Promise<ApiBinaryResult | undefined> {
-		const folders = (await series.folders.getItems()).sort((a, b) => b.level - a.level);
+		const folders = await series.folders.getItems();
+		let p: Folder | undefined = folders[0];
+		while (p) {
+			if (p.folderType === FolderType.artist) {
+				break;
+			}
+			p = await p.parent.get();
+		}
+		if (p) {
+			return this.folderService.getImage(orm, p, size, format);
+		}
 		for (const folder of folders) {
 			const result = this.folderService.getImage(orm, folder, size, format);
 			if (result) {
