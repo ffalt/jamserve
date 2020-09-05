58914112fef4fc9b9b207d977e6d8af466eec9d0
ffalt <ffalty@gmail.com>
2020-09-05 08:54:46 +0200

    feat(log): show graphql query start

--- src/modules/server/middlewares/log.middleware.ts
+++ src/modules/server/middlewares/log.middleware.ts
@@ -6,8 +6,13 @@ const log = logger('Api');
 export function useLogMiddleware(): express.RequestHandler {
 	return (req: express.Request, res: express.Response, next: express.NextFunction): void => {
 		// log all requests
+		let info = '';
+		if (req.originalUrl === '/graphql') {
+			const query = `${req.body?.query}`.slice(0, 50);
+			info = query.slice(0, query.indexOf('{'));
+		}
 		const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		log.access(`${ip} ${req.method} ${req.originalUrl}`);
+		log.access(`${ip} ${req.method} ${req.originalUrl} ${info}`);
 		next();
 	};
 }
