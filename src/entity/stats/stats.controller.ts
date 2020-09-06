1bba2cb1e5e19a2705768130b02b143c35f84a0a
ffalt <ffalty@gmail.com>
2020-09-06 12:35:00 +0200

    docs(REST): update open api spec

--- src/entity/stats/stats.controller.ts
+++ src/entity/stats/stats.controller.ts
@@ -6,13 +6,13 @@ import {Context} from '../../modules/engine/rest/context';
 
 @Controller('/stats', {tags: ['Various'], roles: [UserRole.stream]})
 export class StatsController {
-	@Get(() => Stats, {description: 'Get count Stats for Folders/Tracks/Albums/...', summary: 'Get Stats'})
+	@Get(() => Stats, {description: 'Get count stats for Folders/Tracks/Albums/...', summary: 'Get Stats'})
 	async get(@QueryParams() filter: StatsFilter, @Ctx() {orm, engine}: Context): Promise<Stats> {
 		return await engine.stats.getStats(orm, filter?.rootID);
 	}
 
 	@Get('/user', () => UserStats,
-		{description: 'Get count Stats for the current User: Playlists/Favorites/Played', summary: 'Get USER STATS'})
+		{description: 'Get count stats for the calling User: Playlists/Favorites/Played', summary: 'Get User Stats'})
 	async user(@Ctx() {orm, engine, user}: Context
 	): Promise<UserStats> {
 		return engine.stats.getUserStats(orm, user);
