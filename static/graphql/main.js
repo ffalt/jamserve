5341808ac0c44648c70524356bb96ff1fab60aca
ffalt <ffalty@gmail.com>
2021-11-26 10:32:37 +0100

    bump(deps): bump graphql-playground-react@1.7.28

--- static/graphql/main.js
+++ static/graphql/main.js
@@ -1,27 +1,19 @@
 window.addEventListener('load', function (event) {
-	const loadingWrapper = document.getElementById('loading-wrapper');
-	if (loadingWrapper) {
-		loadingWrapper.classList.add('fadeOut');
-	}
-	const root = document.getElementById('root');
-	root.classList.add('playgroundIn');
-	GraphQLPlayground.init(root,
-		{
-			"endpoint": "/graphql",
-			"subscriptionEndpoint": "/graphql",
-			"version": "1.7.32",
-			"settings": {
-				"general.betaUpdates": false,
-				"editor.theme": "dark",
-				"editor.cursorShape": "line",
-				"editor.reuseHeaders": true,
-				"tracing.hideTracingResponse": true,
-				"queryPlan.hideQueryPlanResponse": true,
-				"editor.fontSize": 14,
-				"editor.fontFamily": "'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace",
-				"request.credentials": "same-origin"
-			},
-			"cdnUrl": "/graphql/playground",
-			"canSaveConfig": false
-		});
+	GraphQLPlayground.init(document.getElementById('root'), {
+		"endpoint": "/graphql",
+		"subscriptionEndpoint": "/graphql",
+		"settings": {
+			"general.betaUpdates": false,
+			"editor.theme": "dark",
+			"editor.cursorShape": "line",
+			"editor.reuseHeaders": true,
+			"tracing.hideTracingResponse": true,
+			"queryPlan.hideQueryPlanResponse": true,
+			"editor.fontSize": 14,
+			"editor.fontFamily": "'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace",
+			"request.credentials": "same-origin"
+		},
+		"cdnUrl": "/graphql/playground",
+		"canSaveConfig": false
+	});
 });
