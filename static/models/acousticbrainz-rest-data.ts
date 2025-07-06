e51b791256a0cc1309eda36153467f30dc8afd75
ffalt <ffalty@gmail.com>
2025-07-06 22:41:54 +0200

    chore(qlty): lint fixes

--- static/models/acousticbrainz-rest-data.ts
+++ static/models/acousticbrainz-rest-data.ts
@@ -7,8 +7,8 @@ export declare namespace AcousticBrainz {
 		metadata?: MetaData;
 	}
 
-	// export type KnownSections = 'danceability' | 'gender' | 'genre_dortmund' | 'genre_electronic' | 'genre_rosamerica' | 'genre_tzanetakis' | 'ismir04_rhythm' | 'mood_acoustic' |
-	// 'mood_aggressive' | 'mood_electronic' | 'mood_happy' | 'mood_party' | 'mood_relaxed' | 'mood_sad' | 'moods_mirex' | 'timbre' | 'tonal_atonal' | 'voice_instrumental';
+	export type KnownSections = 'danceability' | 'gender' | 'genre_dortmund' | 'genre_electronic' | 'genre_rosamerica' | 'genre_tzanetakis' | 'ismir04_rhythm' | 'mood_acoustic' |
+		'mood_aggressive' | 'mood_electronic' | 'mood_happy' | 'mood_party' | 'mood_relaxed' | 'mood_sad' | 'moods_mirex' | 'timbre' | 'tonal_atonal' | 'voice_instrumental';
 
 	export interface HighLevelSection {
 		all: {
