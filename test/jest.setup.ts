52f33fceb4176cc9f738e922cc636c17469caa08
ffalt <ffalty@gmail.com>
2026-05-10 15:46:00 +0200

    test(worker): update error message assertion and ensure mock cleanup in tests

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,7 +1,18 @@
 import 'reflect-metadata';
 import { configureLogger } from '../src/utils/logger.js';
 import nock from 'nock';
+import { afterEach } from '@jest/globals';
 
 configureLogger('none');
 nock.disableNetConnect();
 nock.enableNetConnect(/(localhost|0.0.0.0)/);
+
+afterEach(() => {
+	nock.cleanAll();
+	if (nock.isActive()) {
+		nock.restore();
+	}
+	nock.disableNetConnect();
+	nock.enableNetConnect(/(localhost|0.0.0.0)/);
+});
+
